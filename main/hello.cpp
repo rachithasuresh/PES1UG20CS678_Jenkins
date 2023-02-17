6d3d6759d367a22dd451d0a9ce9621d84351438b
rachithasuresh <rachithas.8@gmail.com>
2023-02-17 21:03:35 +0530

    commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"Change made\n";
+  cout<<"This is the change\n";
   return 0;
 }
