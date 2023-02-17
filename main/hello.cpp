9fd59e93a7c2a714c23f62156c2263ec4139ed40
rachithasuresh <rachithas.8@gmail.com>
2023-02-17 21:37:52 +0530

    4commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,3 +1,4 @@
+
 //File: hello.cpp
 #include <iostream>
 using namespace std;
@@ -6,6 +7,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"This is the changee\n";
   return 0;
 }
--- newfile.cpp
+++ newfile.cpp
@@ -1,1 +1,9 @@
 //File: newfile.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+   return 0;
+}
